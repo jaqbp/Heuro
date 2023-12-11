257b5d8cb7cf872c3e0f82cd2d52a26c907c501b
Jakub Pietrasik <jakubmkjp@gmail.com>
2023-12-11 09:39:11 +0100

    version

--- Backend/calculations/main.py
+++ Backend/calculations/main.py
@@ -7,7 +7,7 @@ import os
 
 def main():
     # test_function = RastriginFunction(lb=np.array([-5.12]), ub=np.array([5.12]), dim=10)
-    test_function = HimmelblauFunction(np.array([-5]), np.array([5]), 3)
+    test_function = HimmelblauFunction(np.array([-5]), np.array([5]), 5)
     goa_algorithm = GOA(SearchAgents_no=20, Max_iter=10)
 
     # for test
